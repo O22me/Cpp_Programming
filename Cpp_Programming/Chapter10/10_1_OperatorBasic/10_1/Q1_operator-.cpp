5cd47d7afa716dfc44717e174646cf5cafa5e375
O22me <sehoon97@naver.com>
2021-01-06 18:19:56 +0900

    ,,

---
+++ Cpp_Programming/Chapter10/10_1_OperatorBasic/10_1/Q1_operator-.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+	int xpos, ypos;
+public:
+	Point(int x, int y) : xpos(x), ypos(y)
+	{
+	}
+	void ShowPosition() const
+	{
+		cout << "[ " << xpos << ", " << ypos << " ]" << endl;
+	}
+	friend Point operator-(const Point& p1, const Point& p2);
+	Point& operator+=(const Point& ref)
+	{
+		xpos += ref.xpos;
+		ypos += ref.ypos;
+		return *this;
+	}
+	Point& operator-=(const Point& ref)
+	{
+		xpos -= ref.xpos;
+		ypos -= ref.ypos;
+		return *this;
+	}
+	friend bool operator==(const Point& p1, const Point& p2);
+};
+
+Point operator-(const Point& p1, const Point& p2)
+{
+	Point pos(p1.xpos - p2.xpos, p1.ypos - p2.ypos);
+	return pos;
+}
+
+bool operator==(const Point& p1, const Point& p2)
+{
+	if ((p1.xpos == p2.xpos) && (p1.ypos == p2.ypos))
+	{
+		return true;
+	}
+	else return false;
+}
+
+int main(void)
+{
+	Point pos1(3, 4);
+	Point pos2(1, 2);
+	Point pos3 = pos1 - pos2;
+	
+	pos1.ShowPosition();
+	pos2.ShowPosition();
+	pos3.ShowPosition();
+	return 0;
+}
